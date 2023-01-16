67a8481a19cd2d117da138e57638069d9f573073
Ahmed Said <ahmed_adnan@stud.cu.edu.eg>
2023-01-16 19:46:52 +0200

    Update main.js
    updated date from 2022 to 2023

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
+let countDownDate = new Date("Dec 31, 2023 23:59:59").getTime();
 // console.log(countDownDate);
 
 let counter = setInterval(() => {
-    // Get Date Now
-    let dateNow = new Date().getTime();
+  // Get Date Now
+  let dateNow = new Date().getTime();
 
-    // Find The Date Difference Between Now And Countdown Date
-    let dateDiff = countDownDate - dateNow;
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
 
-    // Get Time Units
-    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+  // Get Time Units
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
 
-    document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
-    document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
-    document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-    document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
 
-    if (dateDiff < 0) {
-        clearInterval(counter);
-    }
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
 }, 1000);
 
 /*
@@ -37,27 +37,27 @@ let statsSection = document.querySelector(".stats");
 let started = false; // Function Started ? No
 
 window.onscroll = function () {
-    // Skills Animate Width
-    if (window.scrollY >= section.offsetTop - 200) {
-        progressSpans.forEach((span) => {
-            span.style.width = span.dataset.width;
-        });
+  // Skills Animate Width
+  if (window.scrollY >= section.offsetTop - 200) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+  // Stats Increase Number
+  if (window.scrollY >= statsSection.offsetTop - 200) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
     }
-    // Stats Increase Number
-    if (window.scrollY >= statsSection.offsetTop - 200) {
-        if (!started) {
-            nums.forEach((num) => startCount(num));
-        }
-        started = true;
-    }
+    started = true;
+  }
 };
 
 function startCount(el) {
-    let goal = el.dataset.goal;
-    let count = setInterval(() => {
-        el.textContent++;
-        if (el.textContent == goal) {
-            clearInterval(count);
-        }
-    }, 2000 / goal);
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
 }
